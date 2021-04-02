c750ad35c1abff116b1b1f3a1bdd695f7f2e1f89
Raymond Chung <raymond.chung@student.unsw.edu.au>
2021-04-03 03:34:55 +1100

    Minor table changes (new columns)

--- database/hackiethon.sql
+++ database/hackiethon.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user (
     token string NOT NULL,
-    u_id integer NOT NULL,
-    password string NOT NULL,
+    -- u_id integer NOT NULL,
     username string UNIQUE NOT NULL,
+    password string NOT NULL,
     email string UNIQUE NOT NULL,
     name_first string NOT NULL,
     name_last string NOT NULL,
@@ -26,5 +26,3 @@ CREATE TABLE active task (
     foreign key (token) references user(token),
     foreign key (task_id) references tasks(task_id)
 )
-
-select u.token, u.password from user u where u.username = '{}'
