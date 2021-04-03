92a0cd93e91c1199a47b2f7895974052805f2e03
Raymond Chung <raymond.chung@student.unsw.edu.au>
2021-04-03 17:58:06 +1100

    Completed several more routes

--- database/hackiethon.sql
+++ database/hackiethon.sql
@@ -7,22 +7,48 @@ CREATE TABLE user (
     name_first string NOT NULL,
     name_last string NOT NULL,
     level integer,
-    experience integer,
+    xp integer,
     PRIMARY KEY (token)
 )
 
 CREATE table task (
     task_id integer NOT NULL,
-    name string,
+    title string NOT NULL,
     description string,
-    xp integer,
+    task_xp integer,
+    is_custom boolean,
     PRIMARY KEY (task_id)
 )
 
-CREATE TABLE active task (
+CREATE TABLE active_task (
     token string NOT NULL,
     task_id integer NOT NULL,
+    is_completed boolean,
     PRIMARY KEY (token, task_id),
     foreign key (token) references user(token),
     foreign key (task_id) references tasks(task_id)
 )
+
+-- select count(t.task_id) from task t
+
+-- INSERT INTO task (task_id, title, description, xp, is_custom) values ({}, {}, {}, {}, {});
+
+-- BEGIN TRANSACTION;
+-- UPDATE task t
+--    SET  t.title = {},
+--         t.description = {}
+--  WHERE t.id = {};
+-- COMMIT;
+
+-- select u.token from user.u where u.token = {}
+
+-- select t.xp from task t where t.task_id = {};
+
+-- select u.level, u.experience from user u where u.token = '{}';
+
+-- BEGIN TRANSACTION;
+--     UPDATE user u
+--         SET u.level = {},
+--             u.xp = {}
+--     WHERE u.token = {};
+-- COMMIT;
--- server/server.py
+++ server/server.py
@@ -11,6 +11,10 @@ class ConflictError(HTTPException):
     code = 409
     message = 'No message specified'
 
+class NotFound(HTTPException):
+    code = 404
+    message = 'No message specified'
+
 class AccessError(HTTPException):
     code = 403
     message = 'No message specified'
@@ -70,25 +74,109 @@ def auth_register():
 
     hashed_password = hasher(data['password'])
     token = generate_token(data['username'])
-    query = '''INSERT INTO user (token, username, password, email, name_first, name_last) values ({}, {}, {}, {}, {}, {});
+    query = '''BEGIN TRANSACTION;
+                INSERT INTO user (token, username, password, email, name_first, name_last, level, xp) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', 0, 0);
+               COMMIT;
             '''.format(token, data['username'], hashed_password, data['email'], data['name_first'], data['name_last'])
     cur.execute(query)
     return {'token': token}
 
 @app.route('/task/create', methods=['POST'])
 def task_create():
-    return {}
+    data = request.get_json()
+    if data['title'] is None:
+        raise InputError ("Please enter a title")
+    # Generate task_id
+    task_id = 0
+    query = '''select count(t.task_id) from task t;'''
+    cur.execute(query)
+    task_id = cur.fetchone()
+    task_xp = 5
+    # Insert task into database
+    query = '''BEGIN TRANSACTION;
+                INSERT INTO task (task_id, title, description, xp, is_custom) VALUES ({}, '{}', '{}', {}, {});
+               COMMIT;
+            '''.format(task_id, data['title'], data['description'], task_xp, True)
+    cur.execute(query)
+    # # Insert task into user task list
+    # query = '''BEGIN TRANSACTION;
+    #             INSERT INTO active_task (token, task_id) VALUES ({}, {});
+    #            COMMIT;
+    #         '''.format(task_id, data['token'], data['task_id'])
+    # cur.execute(query)
+    return {'task_id': task_id}
 
 @app.route('/task/edit', methods=['PUT'])
 def task_edit():
+    data = request.get_json()
+    if data['title'] is None:
+        raise InputError ("Please enter a title")
+    query = '''BEGIN TRANSACTION;
+                UPDATE task t
+                SET  t.title = '{}',
+                        t.description = '{}'
+                WHERE t.task_id = {};
+                COMMIT;'''.format(data['title'], data['description'], data['task_id'])
+    cur.execute(query)
     return {}
 
 @app.route('/task/remove', methods=['DELETE'])
 def task_remove():
+    data = request.get_json()
+    if data['task_id'] is None:
+        raise NotFound ("Task not found")
+    query = '''BEGIN TRANSACTION;
+                DELETE FROM active_task t
+                WHERE t.task_id = {} and t.token = '{}';
+                COMMIT;'''.format(data['task_id'], data['token'])
+    cur.execute(query)
+    query = '''BEGIN TRANSACTION;
+                DELETE FROM task t
+                WHERE t.task_id = {};
+                COMMIT;'''.format(data['task_id'])
+    cur.execute(query)
     return {}
 
 @app.route('/task/finish', methods=['PUT'])
 def task_finish():
+    xp_threshold = 50
+    new_level = 0
+    new_xp = 0
+    data = request.get_json()
+    if data['token'] is None:
+        raise AccessError ("Invalid Token")
+    if data['task_id'] is None:
+        raise NotFound ("Task not found")
+    query = '''select u.token from user.u where u.token = '{}';'''.format(data['token'])
+    cur.execute(query)
+    x = cur.fetchone()
+    if x is None:
+        raise AccessError ("Invalid Token")
+    query = '''BEGIN TRANSACTION;
+                UPDATE active_task t
+                SET  is_completed = True
+                WHERE t.token = '{}';
+                COMMIT;'''.format(data['token'])
+    cur.execute(query)
+    cur.execute('''select t.task_xp from task t where t.task_id = {};'''.format(data['task_id']))
+    task_xp = cur.fetchone
+    query = '''select u.level, u.xp from user u where u.token = '{}';'''.format(data['token'])
+    x = cur.fetchone()
+    level, xp = x
+    if xp + task_xp >= xp_threshold:
+        new_xp = (xp + task_xp) - xp_threshold
+        new_level = level + 1
+    else:
+        new_xp = xp + task_xp
+        new_level = level
+    query = ''' BEGIN TRANSACTION;
+                    UPDATE user u
+                        SET u.level = {},
+                            u.xp = {}
+                    WHERE u.token = '{}';
+                COMMIT;'''.format(new_level, new_xp, data['token'])
+    cur.execute(query)
+
     return {}
 
 @app.route('/task/gettasks', methods=['GET'])
@@ -102,3 +190,6 @@ def task_finish():
 @app.route('/user/details', methods=['GET'])
 def task_finish():
     return {}
+
+if __name__ == '__main__':
+    app.run()
